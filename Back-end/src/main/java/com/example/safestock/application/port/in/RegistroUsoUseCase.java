50b8d33fad5a112f5df6a6848d575c1b9c05879b
guiguimon200 <guilherme.brito877@sptech.school>
2025-09-05 18:59:10 -0300

    subindo refatoração back end sem os metodos completos

---
+++ Back-end/src/main/java/com/example/safestock/application/port/in/RegistroUsoUseCase.java
@@ -0,0 +1,12 @@
+package com.example.safestock.application.port.in;
+
+import com.example.safestock.domain.model.RegistroUso;
+
+import java.util.List;
+
+public interface RegistroUsoUseCase {
+
+    RegistroUso criar(RegistroUso registroUso);
+    List<RegistroUso> listar();
+    void deletar(Long id);
+}
